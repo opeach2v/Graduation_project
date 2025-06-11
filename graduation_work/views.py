7029b1e525b6ba504da6ff8bcbfebcc9ffcf8b20
opeach2v <opeach2v@naver.com>
2025-06-12 05:32:01 +0900

    fix: name session 만듦

--- graduation_work/views.py
+++ graduation_work/views.py
@@ -113,6 +113,7 @@ def login_user(request):
                 request.session['username'] = user_data.get("username")
                 request.session['role'] = user_data.get("role")
                 request.session['name'] = user_data.get("name")
+                name = user_data.get("name")
             
                 # role에 따라 리디렉션
                 if role == "parent":    # 부모님
