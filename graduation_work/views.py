34e5d914f1c3a14fa1c4add5cca27fe7a3ceb089
opeach2v <opeach2v@naver.com>
2025-07-31 04:11:15 +0900

    print 해봄2

--- graduation_work/views.py
+++ graduation_work/views.py
@@ -646,7 +646,12 @@ def today_chart_data(request, classroom):
     end = datetime.combine(today_kst, time.max)
     end = kst.localize(end)
 
-    print(f"{results_collection.find_one()}")
+    print(f"start: {start}, end: {end}")
+    print(f"child_ids: {child_ids}")
+    print(f"count: {results_collection.count_documents({
+        'child_id': {'$in': child_ids},
+        'timestamp': {'$gte': start, '$lt': end}
+    })}")
     # 1. classroom에 해당하는 아이들 _id 가져오기
     child_docs = children_collection.find({"classroom": classroom})
     child_ids = [doc["_id"] for doc in child_docs]
