014061b7c813481d5d53ff1cc3e8197a14798708
opeach2v <opeach2v@naver.com>
2025-09-04 13:14:27 +0900

    fix: 소수점 아노디게

--- static/js/teachers_page.js
+++ static/js/teachers_page.js
@@ -1,27 +1,29 @@
-let currentChildId;  // 현재 열려있는 알림장의 아이 ID (나중에 알림장 내용할 때 쓸 것)
+let currentChildId; // 현재 열려있는 알림장의 아이 ID (나중에 알림장 내용할 때 쓸 것)
 
 // 개인 알림장 열기
 function openForm(childName, childId) {
-  document.querySelector("#formArea .form-content h3").innerText = `${childName}의 알림장 내용 작성`;
-  currentChildId = childId;  // 현재 열려있는 아이의 ID 저장
-};
+  document.querySelector(
+    "#formArea .form-content h3"
+  ).innerText = `${childName}의 알림장 내용 작성`;
+  currentChildId = childId; // 현재 열려있는 아이의 ID 저장
+}
 
 // 그동안 썼던 알림장 로그 form 열기
 function openLogForm(teacher_id) {
   const container = document.querySelector("#formAreaLog .form-content");
-  const formArea = document.getElementById('formArea');
-  const formAreaLog = document.getElementById('formAreaLog');
+  const formArea = document.getElementById("formArea");
+  const formAreaLog = document.getElementById("formAreaLog");
 
-  container.classList.add('open');
-  formAreaLog.style.width = '400px';
-  formAreaLog.style.padding = '1px';
-  formAreaLog.style.opacity = '1';
-  formAreaLog.style.pointerEvents = 'auto';
+  container.classList.add("open");
+  formAreaLog.style.width = "400px";
+  formAreaLog.style.padding = "1px";
+  formAreaLog.style.opacity = "1";
+  formAreaLog.style.pointerEvents = "auto";
 
-  formArea.style.width = '0';
-  formArea.style.padding = '0';
-  formArea.style.opacity = '0';
-  formArea.style.pointerEvents = 'none';
+  formArea.style.width = "0";
+  formArea.style.padding = "0";
+  formArea.style.opacity = "0";
+  formArea.style.pointerEvents = "none";
 
   // // 알림장 내용 채우기 (AJAX)
   // fetch(`/api/notice_logs/${teacher_id}/`)
@@ -65,101 +67,102 @@ function openLogForm(teacher_id) {
   //     alert("알림장 데이터를 불러오는 데 실패했습니다.");
   //     console.error(err);
   //   });
-};
+}
 
 function closeForm() {
-  const formArea = document.getElementById('formArea');
-  const formAreaLog = document.getElementById('formAreaLog');
-  const container = document.getElementById('homeworkContainer');
+  const formArea = document.getElementById("formArea");
+  const formAreaLog = document.getElementById("formAreaLog");
+  const container = document.getElementById("homeworkContainer");
 
-  formArea.style.display = 'none';
-  formArea.style.width = '0';
-  formArea.style.padding = '0';
-  formArea.style.opacity = '0';
-  formArea.style.pointerEvents = 'none';
+  formArea.style.display = "none";
+  formArea.style.width = "0";
+  formArea.style.padding = "0";
+  formArea.style.opacity = "0";
+  formArea.style.pointerEvents = "none";
 
-  formAreaLog.style.display = 'none';
-  formAreaLog.style.width = '0';
-  formAreaLog.style.padding = '0';
-  formAreaLog.style.opacity = '0';
-  formAreaLog.style.pointerEvents = 'none';
+  formAreaLog.style.display = "none";
+  formAreaLog.style.width = "0";
+  formAreaLog.style.padding = "0";
+  formAreaLog.style.opacity = "0";
+  formAreaLog.style.pointerEvents = "none";
 
-  container.classList.remove('open');
+  container.classList.remove("open");
 }
 
 // 개인 알림장 내용 저장 (AJAX)
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("writeNotice");
   if (form) {
-    form.addEventListener("submit", function(e) {
+    form.addEventListener("submit", function (e) {
       e.preventDefault(); // 폼 기본 동작(페이지 이동) 막기
 
-      
       const content = document.getElementById("noticeContent").value;
-      const csrfToken = document.querySelector("[name=csrfmiddlewaretoken]").value;
-      const url = form.dataset.url;  // form 요소의 data-url 속성 값 읽기
-      
+      const csrfToken = document.querySelector(
+        "[name=csrfmiddlewaretoken]"
+      ).value;
+      const url = form.dataset.url; // form 요소의 data-url 속성 값 읽기
+
       console.log(`아이 ID: ${currentChildId}, ${url}`); // 디버깅용 로그
 
       fetch(url, {
         method: "POST",
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
-          "X-CSRFToken": csrfToken
+          "X-CSRFToken": csrfToken,
         },
-        body: new URLSearchParams({ 
-          content : content,
-          child_id: currentChildId  // 현재 열려있는 아이의 ID 전달
+        body: new URLSearchParams({
+          content: content,
+          child_id: currentChildId, // 현재 열려있는 아이의 ID 전달
+        }),
+      })
+        .then((res) => {
+          if (!res.ok) {
+            // 응답 상태가 200 OK가 아니면
+            throw new Error(`서버 오류: ${res.status}`);
+          }
+          return res.json();
         })
-      })
-      .then(res => {
-        if (!res.ok) {
-          // 응답 상태가 200 OK가 아니면
-          throw new Error(`서버 오류: ${res.status}`);
-        }
-        return res.json();
-      })
-      .then(data => {
-        if (data.error) {
-          alert("데이터 에러" + data.error);
-        } else {
-          alert("저장되었습니다!");
-          form.querySelector('textarea').value = '';  // 저장 후 textarea 초기화
-        }
-      })
-      .catch(err => {
-        alert(`저장 중 오류 발생: ${err.message}`);
-        console.error(err);
-      });
+        .then((data) => {
+          if (data.error) {
+            alert("데이터 에러" + data.error);
+          } else {
+            alert("저장되었습니다!");
+            form.querySelector("textarea").value = ""; // 저장 후 textarea 초기화
+          }
+        })
+        .catch((err) => {
+          alert(`저장 중 오류 발생: ${err.message}`);
+          console.error(err);
+        });
     });
   }
 });
 
 // 개인 알림장 내용 삭제(textarea 비우기)
 function clearForm() {
-  const formArea = document.getElementById('formArea');
-  formArea.querySelector('textarea').value = '';
+  const formArea = document.getElementById("formArea");
+  formArea.querySelector("textarea").value = "";
 }
 
 // js로 뒤로가기 감지해서 막기
-window.onload = function() {
-    if (!'{{ request.session.username|default_if_none:"" }}') {
-      window.location.href = "{% url 'login_user' %}";
-    }
-  };
+window.onload = function () {
+  if (!'{{ request.session.username|default_if_none:"" }}') {
+    window.location.href = "{% url 'login_user' %}";
+  }
+};
 
+window.history.pushState(null, "", window.location.href);
+window.onpopstate = function () {
   window.history.pushState(null, "", window.location.href);
-  window.onpopstate = function () {
-    window.history.pushState(null, "", window.location.href);
-  };
+};
 
 // 도넛 그래프 동적으로 그리기 (전체 데이터)
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   const classroom = document.getElementById("doughnut-box").dataset.classroom;
 
   fetch(`/chart-data/${classroom}`)
-    .then(response => response.json())
-    .then(chartInfo => {
+    .then((response) => response.json())
+    .then((chartInfo) => {
       const totalEvents = chartInfo.data.reduce((sum, val) => sum + val, 0);
       const chartContainer = document.getElementById("doughnut-box");
 
@@ -168,26 +171,31 @@ document.addEventListener('DOMContentLoaded', function () {
       if (totalEvents === 0) {
         // 데이터가 없을 때 <p> 메시지 삽입
         chartContainer.innerHTML = `<p>해당 반에서 위험 행동은 없습니다.</p>`;
-      }
-      else {
+      } else {
         new Chart(document.getElementById("doughnut-chart"), {
-          type: 'doughnut',
+          type: "doughnut",
           data: {
             labels: chartInfo.labels,
-            datasets: [{
-              label: "event",
-              backgroundColor: [
-                "#d62827", "#94d2bb", "#099396", "#00304e", "#f58000",
-              ],
-              data: chartInfo.data
-            }]
+            datasets: [
+              {
+                label: "event",
+                backgroundColor: [
+                  "#d62827",
+                  "#94d2bb",
+                  "#099396",
+                  "#00304e",
+                  "#f58000",
+                ],
+                data: chartInfo.data,
+              },
+            ],
           },
           options: {
             title: {
               display: true,
-              text: '반 전체 행동 분석 (건)'
-            }
-          }
+              text: "반 전체 행동 분석 (건)",
+            },
+          },
         });
 
         const totalEvents = chartInfo.data.reduce((sum, val) => sum + val, 0);
@@ -197,7 +205,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
         if (fightingIndex !== -1 && totalEvents > 0) {
           const fightingCount = chartInfo.data[fightingIndex];
-          const fightingRatio = ((fightingCount / totalEvents) * 100).toFixed(1);
+          const fightingRatio = ((fightingCount / totalEvents) * 100).toFixed(
+            1
+          );
 
           countMessage = `위험 행동(fighting)/전체 행동: ${fightingCount}/${totalEvents}`;
           ratioMessage = `위험 행동(fighting) 비율: ${fightingRatio}%`;
@@ -229,18 +239,21 @@ document.addEventListener('DOMContentLoaded', function () {
         }
       }
     })
-    .catch(error => {
+    .catch((error) => {
       console.error("차트 데이터를 불러오는 중 오류 발생:", error);
     });
 });
 
 // 바 그래프 동적으로 그리기 (당일 데이터)
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   const classroom = document.getElementById("bar-box").dataset.classroom;
   fetch(`/today-chart-data/${classroom}`)
-    .then(response => response.json())
-    .then(chartInfo => {
-      const totalEvents = chartInfo.todayData.reduce((sum, val) => sum + val, 0);
+    .then((response) => response.json())
+    .then((chartInfo) => {
+      const totalEvents = chartInfo.todayData.reduce(
+        (sum, val) => sum + val,
+        0
+      );
       const chartContainer = document.getElementById("bar-box");
 
       console.log("당일 데이터 Chart Info:", chartInfo); // 디버깅용 로그
@@ -248,37 +261,48 @@ document.addEventListener('DOMContentLoaded', function () {
       if (totalEvents === 0) {
         // 데이터가 없을 때 <p> 메시지 삽입
         chartContainer.innerHTML = `<p>오늘 하루 위험 행동은 없습니다.</p>`;
-      }
-      else {
+      } else {
         new Chart(document.getElementById("bar-chart"), {
-          type: 'bar',
+          type: "bar",
           data: {
             labels: chartInfo.todayLabels,
-            datasets: [{
-              label: "event",
-              backgroundColor: [
-                'rgba(247, 46, 46, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(255, 205, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-              ],
-              borderColor: [
-                'rgb(252, 83, 83)',
-                'rgb(255, 159, 64)',
-                'rgb(255, 205, 86)',
-                'rgb(75, 192, 192)',
-                'rgb(54, 162, 235)',
-              ],
-              data: chartInfo.todayData
-            }]
+            datasets: [
+              {
+                label: "event",
+                backgroundColor: [
+                  "rgba(247, 46, 46, 0.2)",
+                  "rgba(255, 159, 64, 0.2)",
+                  "rgba(255, 205, 86, 0.2)",
+                  "rgba(75, 192, 192, 0.2)",
+                  "rgba(54, 162, 235, 0.2)",
+                ],
+                borderColor: [
+                  "rgb(252, 83, 83)",
+                  "rgb(255, 159, 64)",
+                  "rgb(255, 205, 86)",
+                  "rgb(75, 192, 192)",
+                  "rgb(54, 162, 235)",
+                ],
+                data: chartInfo.todayData,
+              },
+            ],
           },
           options: {
-            title: {
-              display: true,
-              text: '오늘의 행동 분석 (건)'
-            }
-          }
+            plugins: {
+              title: {
+                display: true,
+                text: "오늘의 행동 분석 (건)",
+              },
+            },
+            scales: {
+              y: {
+                beginAtZero: true,
+                ticks: {
+                  precision: 0,
+                },
+              },
+            },
+          },
         });
         const chartContainer = document.getElementById("bar-box");
 
@@ -303,7 +327,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
       }
     })
-    .catch(error => {
+    .catch((error) => {
       console.error("차트 데이터를 불러오는 중 오류 발생:", error);
     });
 });
